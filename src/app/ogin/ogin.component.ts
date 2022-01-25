3b8de6fe2aa714df68ea5f9f6de47bad8e9839d2
SheethalKhan <sheethalkhans06@gmail.com>
2022-01-25 17:17:11 +0530

    2 files modified, 1 added

--- src/app/ogin/ogin.component.ts
+++ src/app/ogin/ogin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Validation } from '../validation';
 
 @Component({
   selector: 'app-ogin',
@@ -21,11 +22,27 @@ export class OginComponent implements OnInit {
           Validators.pattern('^[0-9]+$'),
           Validators.minLength(5),
         ],
+        
       ],
       email: ['', [Validators.required, Validators.email]],
-    });
+      confirmpassword:['',Validators.required]
+    },{Validator:Validation('password','confirmpassword')});
   }
   login() {
     this.route.navigate(['list']);
+  }
+  confirmPassword(controlName: string, matchingControlName: string){
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ confirmPassword: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
   }
 }
